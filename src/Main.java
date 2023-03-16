a942d057679d84943aa47b10d53509940dc6d8b8
lukyjuranek <lukasjuranek382@gmail.com>
2023-03-16 13:09:12 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,75 @@
+public class Main {
+    public static void main(String[] args) {
+//        array of users
+//        array of songs
+
+
+        System.out.println("Welcome to singing contest organizer");
+
+//        Create a main loop that will run until the user exits the program
+//        The loop should ask the user to enter the password
+//        If the password is correct, it should show the menu
+//        If the password is incorrect, it should ask the user to enter the password again
+        while(true) {
+            System.out.println("Please authenticate");
+//            Ask the user to enter the username
+//            Ask the user to enter the password
+
+//            if the username and password are correct, show the menu until the user exits the program or signs out
+        }
+}
+    public void checkPassword(String password) {
+        /** Checks the password
+         * @param password
+         */
+
+    }
+
+    public void showMenu() {
+        /** Prints the menu based on the user role */
+    }
+
+    public void showSingers() {
+        /** Prints the singers */
+    }
+
+    public void showVoters() {
+        /** Prints the voters */
+    }
+
+    public void showVotes() {
+        /** Prints the votes */
+    }
+
+    public void addSongOrGenre() {
+        /** Adds a song or a genre
+         * Shows the list
+         */
+    }
+
+    public void voteSong() {
+        /** Asks the user to vote for a song */
+    }
+
+    public void registerUser() {
+        /** Registers the user */
+    }
+
+    public void updateUserData() {
+        /** Lets you select the user and update the data */
+    }
+
+    public void deleteUser() {
+        /** Lets you select the user and delete the user */
+    }
+
+    public void registerSinger() {
+        /** Registers the singer */
+    }
+
+    public void registerVoter() {
+        /** Registers the voter */
+    }
+
+
+}
